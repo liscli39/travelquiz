cd90aae41456c6404ee8735143eeded0267a17d0
liscli39 <suppo1999@gmail.com>
2022-06-18 00:32:16 +0700

    Update

--- app/models/question.py
+++ app/models/question.py
@@ -3,7 +3,7 @@ from app.models.week import Week
 
 class Question(models.Model):
     def __str__(self):
-        return (self.question_text[:75] + '..') if len(self.question_text) > 75 else self.question_text
+        return (self.question_text[:60] + '..') if len(self.question_text) > 60 else self.question_text
 
     question_id = models.BigAutoField(primary_key=True)
     question_text = models.TextField()
